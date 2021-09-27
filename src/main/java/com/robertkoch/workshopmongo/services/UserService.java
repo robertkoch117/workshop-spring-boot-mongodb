1433a0feb8635cb4601603e86485098265c6ca5a
Robert Koch <robertkoch117@gmail.com>
2021-09-27 08:50:07 -0300

    Obtendo um usuario por id

--- src/main/java/com/robertkoch/workshopmongo/services/UserService.java
+++ src/main/java/com/robertkoch/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.robertkoch.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.robertkoch.workshopmongo.domain.User;
 import com.robertkoch.workshopmongo.repository.UserRepository;
+import com.robertkoch.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,13 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		/*if(user == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado");
+		}*/
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
