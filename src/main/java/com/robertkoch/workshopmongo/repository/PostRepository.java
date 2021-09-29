1c7c3af177ce432a83aa85ac03289d013fb2d35f
Robert Koch <robertkoch117@gmail.com>
2021-09-29 08:38:12 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/robertkoch/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.robertkoch.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.robertkoch.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+	
+}
